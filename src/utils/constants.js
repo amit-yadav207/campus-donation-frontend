5c0061bfa62b89352b933af17a4964e84ce41983
Amit <amit.yd725@gmail.com>
2024-11-14 02:57:14 +0530

    changed backend url as deployed backend on vercel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 // baseUrl.js
 // const BASE_URL = 'http://localhost:5000';
-const BASE_URL='https://campus-donation-backend.onrender.com';
+// const BASE_URL='https://campus-donation-backend.onrender.com';
+const BASE_URL='https://campus-donation-backend.vercel.app'
 const REACT_APP_BACKEND_URL='http://localhost:5000'
 export { BASE_URL,REACT_APP_BACKEND_URL};
 
